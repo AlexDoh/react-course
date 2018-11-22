8151ea7408d91229eb7735ce93b5ea58cbf2edfa
odmy <odmitrenko1@gmail.com>
2018-11-22 17:48:11 +0200

    Made ArticleTitle functional component.

--- src/components/article/article-title/ArticleTitle.tsx
+++ src/components/article/article-title/ArticleTitle.tsx
@@ -1,39 +1,15 @@
 import * as React from 'react';
-import { Component, ComponentProps, ComponentState } from 'react';
 import Button from '../../../shared/button/Button';
 import '../../../assets/scss/components/ArticleTitle.scss';
 
-class ArticleTitle extends Component<ComponentProps<any>, ComponentState> {
-  constructor(props) {
-    super(props);
-
-    const { onArticleToggle, label, showArticle } = props;
-
-    this.state = {
-      onArticleToggle,
-      label,
-      showArticle
-    };
-  }
-
-  showArticleLabel: string = 'show article';
-  hideArticleLabel: string = 'hide article';
-
-  componentDidMount = (): void => {
-  };
-
-  componentWillUnmount = (): void => {
-  };
-
-  render = (): JSX.Element => (
+const ArticleTitle = ({ onArticleToggle, label, showArticle }): JSX.Element => (
     <div className='container'>
-      <h2 className='container__title'>{this.state.label}</h2>
+      <h2 className='container__title'>{label}</h2>
       <Button
-        onButtonClick={this.state.onArticleToggle}
-        label={this.state.showArticle ? this.showArticleLabel : this.hideArticleLabel}
+        onButtonClick={onArticleToggle}
+        label={showArticle ? 'show article' : 'hide article'}
       />
     </div>
   );
-}
 
 export default ArticleTitle;
